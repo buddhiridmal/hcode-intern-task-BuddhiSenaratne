a507f246be140a2bfc636dca46232cde0162b290
Buddhi Ridmal <buddhiridmal@gmail.com>
2025-06-03 19:04:02 +0530

    Initial commit – setup NestJS with PostgreSQL and Vehicles module

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { Vehicle } from './vehicles/vehicle.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Vehicle],
+      synchronize: true,
+    }),
+    VehiclesModule,
+  ],
+})
+export class AppModule {}
